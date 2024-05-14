415d050b19594d455099b4db275405cb37ef4608
yungwine <cyrbatoff@gmail.com>
2024-05-14 12:19:04 +0700

    add app title

--- src/app/api.py
+++ src/app/api.py
@@ -3,5 +3,5 @@ from fastapi import FastAPI
 from . import endpoints
 
 
-app = FastAPI()
+app = FastAPI(title='Trace emulator', description='Trace emulator for TON Blockchain.')
 app.include_router(endpoints.router)
