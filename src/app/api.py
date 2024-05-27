610ed6bac6c758d0120edab4ac6d31735bb7d252
yungwine <cyrbatoff@gmail.com>
2024-05-27 13:25:05 +0700

    add grafana

--- src/app/api.py
+++ src/app/api.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-
+from prometheus_fastapi_instrumentator import Instrumentator
 
 from . import endpoints
 
 
 app = FastAPI(title='Trace emulator', description='Trace emulator for TON Blockchain.')
 app.include_router(endpoints.router)
+Instrumentator(
+    env_var_name="ENABLE_METRICS"
+).instrument(app).expose(app)
